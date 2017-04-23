57d0910998b282dbe80f4a9543ba5c35a19c68eb
mack <mack@MacBook-Pro.local>
2017-04-23 16:36:09 +0100

    first commit

---
+++ files/modernizr-2.8.3.min.js
@@ -0,0 +1,135 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+		
+        <meta name="description" content="">
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0" />
+
+		<link rel="stylesheet" href="http://simonpan.com/wp-content/themes/sp_portfolio/style.css" />
+		<link rel="pingback" href="http://simonpan.com/xmlrpc.php" />
+		<link rel="shortcut icon" href="http://simonpan.com/wp-content/themes/sp_portfolio/style.css/favicon.ico" />
+         
+        
+        
+        
+<!-- This site is optimized with the Yoast SEO plugin v3.2.5 - https://yoast.com/wordpress/plugins/seo/ -->
+<title>Page Not Found - User Experience Design Portfolio of Simon Pan</title>
+<meta name="robots" content="noindex,follow"/>
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="object" />
+<meta property="og:title" content="Page Not Found - User Experience Design Portfolio of Simon Pan" />
+<meta property="og:site_name" content="User Experience Design Portfolio of Simon Pan" />
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:title" content="Page Not Found - User Experience Design Portfolio of Simon Pan" />
+<meta name="twitter:site" content="@span870" />
+<!-- / Yoast SEO plugin. -->
+
+<link rel='dns-prefetch' href='//ajax.googleapis.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/simonpan.com\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.7.3"}};
+			!function(a,b,c){function d(a){var b,c,d,e,f=String.fromCharCode;if(!k||!k.fillText)return!1;switch(k.clearRect(0,0,j.width,j.height),k.textBaseline="top",k.font="600 32px Arial",a){case"flag":return k.fillText(f(55356,56826,55356,56819),0,0),!(j.toDataURL().length<3e3)&&(k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,65039,8205,55356,57096),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,55356,57096),0,0),c=j.toDataURL(),b!==c);case"emoji4":return k.fillText(f(55357,56425,55356,57341,8205,55357,56507),0,0),d=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55357,56425,55356,57341,55357,56507),0,0),e=j.toDataURL(),d!==e}return!1}function e(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i,j=b.createElement("canvas"),k=j.getContext&&j.getContext("2d");for(i=Array("flag","emoji4"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='fv_flowplayer-css'  href='http://simonpan.com/wp-content/fv-flowplayer-custom/style-1.css?ver=1464924084' type='text/css' media='all' />
+<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js'></script>
+<script type='text/javascript' src='http://simonpan.com/wp-content/themes/sp_portfolio/js/modernizr.custom.min.js?ver=version'></script>
+<link rel='https://api.w.org/' href='http://simonpan.com/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://simonpan.com/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://simonpan.com/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 4.7.3" />
+        
+    </head>
+    	
+	<body id=work class=im-joining-medium>
+		
+							
+			<!--[if lt IE 8]>
+			    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+			<![endif]-->
+			
+			<header id="site-header" class="m-all t-all d-all ld1 xld1">
+				<section id="masthead-wrapper">
+					<div class="logo-wrapper m2 t2-t7 d1-d3">
+													<a class="logo" id="logo" href="/">
+								<img src="http://simonpan.com/wp-content/themes/sp_portfolio/assets/sp-logo.png" alt="Simon Pan Monogram Logo" />
+							</a>
+					</div>
+					<p class="micro m2 t2-t7 d4-d6 ld-hide xld-hide">Simon Pan is a Product Designer based in San Francisco.</p>
+					<nav class="m2 t2-t7 d7-d10"><ul id="menu-main" class="menu"><li id="menu-item-808" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-808"><a href="index.html">Portfolio</a></li>
+<li id="menu-item-63" class="menu-item menu-item-type-post_type menu-item-object-page current_page_parent menu-item-63"><a href="http://simonpan.com/thoughts/">Writing</a></li>
+<li id="menu-item-807" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-807"><a href="http://simonpan.com/about/">About</a></li>
+</ul></nav>				</section>
+			</header>
+			
+			<!-- End Header -->
+			
+			<!-- Main Area -->	
+<div class="main-content light m-all t-all d-all ld2-ld9 xld2-xld9">
+	<section id="404" class="phase case-block m-all t-all d-all ld-all xld-all">
+		<figure class="tight m2 m-padinfull t4-t5 d5-d6 ld5-ld6 xld5-xld6">
+			<img src="http://simonpan.com/wp-content/themes/sp_portfolio/assets/sad_panda.png" alt="A sad panda holding a sign that reads no!" />
+		</figure>
+		<div class="center m2 t3-t6 d4-d7 ld4-ld7 xld4-xld7">
+			<p>Sorry, the page you were looking for could not be found. The page may have moved or it no longer exists.</p>
+			<p>Try checking the URL or <a href="/">return home</a>.</p>
+		</div>
+		
+		
+	</section>
+</div>
+			
+			
+			
+
+		<footer id="contact" class="m2 t-all d-all ld2-ld9 xld2-xld9">
+			<ul class="links m-padinfull d2-d6 ld2-ld5 xld2-xld5">
+				<li><a href="mailto:simon.pan@me.com">simon.pan@me.com</a></li>
+				<li><a href="https://twitter.com/5imonPan" target="_blank">Twitter</a></li>
+				<li><a href="http://www.linkedin.com/pub/simon-pan/21/54b/a04" target="_blank">LinkedIn</a></li>
+				<li><a href="http://dribbble.com/simonpan" target="_blank">Dribbble</a></li>
+				<li><a href="https://www.pinterest.com/simonshirazpan/" target="_blank" class="last" href="">Pinterest</a></li>
+			</ul>
+			<p class="copyright nano d7-d9 ld8-ld9 xld8-xld9">Website design and content &copy; 2017 Simon Pan.</p>
+		</footer>
+		
+		<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+	    <script src="js/plugins.js"></script>
+	    <script src="js/main.js"></script>
+		<script src="https://get.gridsetapp.com/33707/overlay/"></script>
+		
+		<script>
+		    var _gaq=[['_setAccount','UA-22788983-1'],['_trackPageview']];
+		    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+		    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+		    s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+		
+	<script type='text/javascript' src='http://simonpan.com/wp-content/themes/sp_portfolio/js/jquery.randomContent.min.js?ver=version'></script>
+<script type='text/javascript' src='http://simonpan.com/wp-content/themes/sp_portfolio/js/wz_tooltip.min.js?ver=version'></script>
+<script type='text/javascript' src='http://simonpan.com/wp-content/themes/sp_portfolio/js/engage.lastfm.min.js?ver=version'></script>
+<script type='text/javascript' src='http://simonpan.com/wp-content/themes/sp_portfolio/js/jquery.floatingFixed.min.js?ver=version'></script>
+<script type='text/javascript' src='http://simonpan.com/wp-content/themes/sp_portfolio/js/jquery.scrollTo.min.js?ver=version'></script>
+<script type='text/javascript' src='http://simonpan.com/wp-content/themes/sp_portfolio/js/jquery.nav.min.js?ver=version'></script>
+<script type='text/javascript' src='http://simonpan.com/wp-includes/js/wp-embed.min.js?ver=4.7.3'></script>
+ 
+		
+	</body>
+	</html>
